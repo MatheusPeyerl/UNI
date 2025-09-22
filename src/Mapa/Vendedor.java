0369978d69f11ebf42989449f227b250bdaf0343
MatheusPeyerl <mgpeyerl@hotmail.com>
2025-09-22 13:56:13 -0300

    Update Vendedor.java

--- src/Mapa/Vendedor.java
+++ src/Mapa/Vendedor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Mapa;
 
 /**
@@ -10,24 +5,25 @@ package Mapa;
  * @author Matheus
  */
 public class Vendedor extends Funcionario {
-    
-     double venda = 2000;
-     
 
-  
-    public void Funcionario(){
-        
-    } 
-       
-      @Override
-    public void exibeDados(){
-        System.out.print("\n----Vendedor----");
-        super.exibeDados();
-        System.out.println("Salario com comissão: "+(getSalario()+venda*0.3));
-       
+    private double venda = 2000;
+
+    public Vendedor() {
+        super();
     }
-            
-    
-    
-    
+
+    @Override
+    public void exibeDados() {
+        System.out.println("\n---- Vendedor ----");
+        super.exibeDados();
+        System.out.println("Salário com comissão: " + (getSalario() + venda * 0.3));
+    }
+
+    public double getVenda() {
+        return venda;
+    }
+
+    public void setVenda(double venda) {
+        this.venda = venda;
+    }
 }
