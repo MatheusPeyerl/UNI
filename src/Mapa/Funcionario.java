0fac887e8d81924980ae5bcc0256cd7e5e6f8a12
MatheusPeyerl <mgpeyerl@hotmail.com>
2025-09-22 13:53:47 -0300

    Update Funcionario.java
    Fixed indentation due the file being old and the IDE variations

--- src/Mapa/Funcionario.java
+++ src/Mapa/Funcionario.java
@@ -1,27 +1,23 @@
-
 package Mapa;
 
 /**
  *
  * @author Matheus
  */
-public abstract class  Funcionario extends Pessoa {
-    
+public abstract class Funcionario extends Pessoa {
+
     protected double salario;
     protected int ramal;
     protected double anual;
-    
 
-    public Funcionario(){
-        
+    public Funcionario() {
     }
-    
 
     public void setSalario(double salario) {
         this.salario = salario;
     }
-        
-        public double getSalario() {
+
+    public double getSalario() {
         return salario;
     }
 
@@ -32,29 +28,24 @@ public abstract class  Funcionario extends Pessoa {
     public void setRamal(int ramal) {
         this.ramal = ramal;
     }
-    
-    public double setCalculoSalarioAnual(double anual){
-        this.anual+=anual*13;
+
+    public double setCalculoSalarioAnual(double anual) {
+        this.anual += anual * 13;
         return anual;
-        
     }
-    public double getCalculoSalarioAnual(){
-        System.out.println("Salario anual:" +this.anual );
+
+    public double getCalculoSalarioAnual() {
+        System.out.println("Salario anual: " + this.anual);
         return 0;
     }
-    
-    
-    
+
     @Override
-        public void exibeDados()  {    
-        System.out.println("\nNome: "+this.nome+"\nDocumento: "+this.documento);
-        System.out.println("salario: "+this.salario+"\nramal: "+this.ramal);
-               
-    
-   
-}
+    public void exibeDados() {
+        System.out.println("\nNome: " + this.nome + "\nDocumento: " + this.documento);
+        System.out.println("Salario: " + this.salario + "\nRamal: " + this.ramal);
+    }
 
     void exbideDados() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 }
